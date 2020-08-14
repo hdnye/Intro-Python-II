a1fe423dd32f1b0166d54fc4d8e98ec577423ed6
Heather Nye <heatherdnye@gmail.com>
2020-08-13 19:39:24 -0500

    game works

--- src/adv.py
+++ src/adv.py
@@ -5,20 +5,20 @@ from player import Player
 
 # Dictionary of rooms mapping name to Room 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    "outside":  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    "foyer":    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    "overlook": Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    "narrow":   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    "treasure": Room("Treasure Chamber", """You"ve found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -26,22 +26,22 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room["outside"].n_to = room["foyer"]
+room["foyer"].s_to = room["outside"]
+room["foyer"].n_to = room["overlook"]
+room["foyer"].e_to = room["narrow"]
+room["overlook"].s_to = room["foyer"]
+room["narrow"].w_to = room["foyer"]
+room["narrow"].n_to = room["treasure"]
+room["treasure"].s_to = room["narrow"]
 
 #
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
+# Make a new player object that is currently in the "outside" room.
 
-player1 = Player(room['outside'])
+player1 = Player(room["outside"])
    
 
 # Write a loop that:
@@ -51,32 +51,35 @@ player1 = Player(room['outside'])
 # * Waits for user input and decides what to do.
 while True: 
     current_room = player1.current_room
-    print('player1', player1.current_room.name) 
-    print('player1', player1.current_room.description)
+    print("player1", player1.current_room.name) 
+    print("player1", player1.current_room.description)
     user_input = input("Choose a direction to move in ('n', 's', 'e', 'w'):\n")    
-    attribute = user_input + '_to' 
+    attribute = user_input + "_to"   
 # If the user enters a cardinal direction, attempt to move to the room there.
-    if user_input == 'n':
-        if hasattr(current_room, 'n_to'):
-            player1.current_room = getattr(current_room, 's_to')  
-    if user_input == 's':    
-        if hasattr(current_room, 's_to'):
-            player1.current_room = getattr(current_room, 'n_to')
-    if user_input == 'e':
-        if hasattr(current_room, 'e_to'):
-            player1.current_room = getattr(current_room, 'w_to')
-    if user_input == 'w':         
-        if hasattr(current_room, 'w_to'):
-            player1.current_room = getattr(current_room, 'e_to')
-# Print an error message if the movement isn't allowed.
-        else:
-            print('That is not a valid direction, please choose again')
-# If the user enters "q", quit the game.
-        if user_input == 'q':
-            break
+    if user_input == "n":
+        if hasattr(current_room, "n_to"):
+            player1.current_room = getattr(current_room, attribute)                    
+    if user_input == "s":    
+        if hasattr(current_room, "s_to"):
+            player1.current_room = getattr(current_room, attribute)
+    if user_input == "e":
+        if hasattr(current_room, "e_to"):
+            player1.current_room = getattr(current_room, attribute)
+    if user_input == "w":         
+        if hasattr(current_room, "w_to"):
+            player1.current_room = getattr(current_room, attribute)
+    if user_input == "q":
+            break        
+# # Print an error message if the movement isn"t allowed.
+    else:
+        print("That is not a valid direction, please choose again")
+# # If the user enters "q", quit the game.
+   
  
  # PLAN
 # 1) Create room class with name and description
 # rooms are already linked
 # 2) create a player object
 # 3) figure out movement hasattr getattr
+
+
--- src/room.py
+++ src/room.py
@@ -5,10 +5,7 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.n_to: Room = None
-        self.s_to: Room = None
-        self.e_to: Room = None
-        self.w_to: Room = None
+     
 
     
 
